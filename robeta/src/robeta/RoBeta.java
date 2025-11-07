0f5fae77f1f0ff9778fc3a34ad6f0f5d5319ae36
Kaio0708 <kaiovitor0707@gmail.com>
2025-11-07 00:22:17 -0300

    Adição de projeto RoBeta

---
+++ robeta/src/robeta/RoBeta.java
@@ -0,0 +1,53 @@
+package robeta;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class RoBeta {
+	private int bateria;
+	private String[] palavras;
+	
+	public RoBeta(int bateria, String [] palavras) {
+		if(palavras == null) {
+			throw new NullPointerException();
+		}
+		this.bateria = bateria;
+		this.palavras = palavras;
+	}
+	
+	public String exibeRobo() {
+		return "RoBeta - " + this.bateria + " - " + Arrays.toString(palavras);
+	}
+	
+	public String falar() {
+		Random random = new Random();
+		int index = random.nextInt(palavras.length);
+		return falar(index);
+	}
+	
+	public String falar(int i) {
+		if(i < 0 || i >= palavras.length) {
+			return "Palavras não existe";
+		} if(consomeEnergia()){
+			return palavras[i];
+		} else {
+			return "Estou sem bateria";
+		}
+	}
+	
+	private boolean consomeEnergia() {
+		if(bateria > 0) {
+			bateria --;
+			return true;
+		}
+		return false;
+	}
+	
+	public int getBateria() {
+		return bateria;
+	}
+	
+	public void setBateria(int bateria) {
+		this.bateria = bateria;
+	}
+}
