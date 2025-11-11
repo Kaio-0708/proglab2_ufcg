e28784fe4f7a29e966c41eec02a60cb4af436db3
Kaio0708 <kaiovitor0707@gmail.com>
2025-11-10 21:31:56 -0300

    Implementação da classe "RegistroTempoOnline".

--- src/br/edu/ufcg/computacao/p2lp2/coisa/RegistroTempoOnline.java
+++ src/br/edu/ufcg/computacao/p2lp2/coisa/RegistroTempoOnline.java
@@ -1,5 +1,32 @@
 package br.edu.ufcg.computacao.p2lp2.coisa;
 
 public class RegistroTempoOnline {
-
+	private int tempoOnline;
+	private int tempoOnlineEsperado;
+	private String nomeDisciplina;
+	
+	public RegistroTempoOnline(String nomeDisciplina) {
+		this(nomeDisciplina, 120);
+	}
+	
+	public RegistroTempoOnline(String nomeDisciplina, int tempoOnlineEsperado) {
+		this.nomeDisciplina = nomeDisciplina;
+		this.tempoOnlineEsperado = tempoOnlineEsperado;
+	}
+	
+	public void adicionaTempoOnline(int tempo) {
+		tempoOnline += tempo;
+	}
+	
+	public boolean atingiuMetaTempoOnline() {
+		if(tempoOnline >= tempoOnlineEsperado) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public String toString() {
+		return nomeDisciplina + " " + tempoOnline + "/" + tempoOnlineEsperado;
+	}
 }
