f43d0ecfdf501afb6b765681c6f5edc0af3dc7a4
Kaio0708 <kaiovitor0707@gmail.com>
2025-11-12 10:25:04 -0300

    Implementação da classe "Disciplina"

--- src/br/edu/ufcg/computacao/p2lp2/coisa/Disciplina.java
+++ src/br/edu/ufcg/computacao/p2lp2/coisa/Disciplina.java
@@ -1,5 +1,46 @@
 package br.edu.ufcg.computacao.p2lp2.coisa;
 
-public class Disciplina {
+import java.util.*;
 
+public class Disciplina {
+	private String nomeDisciplina;
+	
+	private int horas;
+	
+	private double[] notas;
+	
+	public Disciplina(String nomeDisciplina) {
+		this.nomeDisciplina = nomeDisciplina;
+		this.notas = new double[4];
+	}
+	
+	public void cadastraHoras(int horas) {
+		this.horas += horas;
+	}
+	
+	public void cadastraNota(int nota, double valorNota) {
+		notas[nota - 1] = valorNota;
+	}
+	
+	private double media() {
+		double soma = 0;
+		for (int i = 0; i < notas.length; i++){
+			soma += notas[i];
+		}
+		
+		return soma / notas.length;
+	}
+	
+	public boolean aprovado() {
+		if(media() >= 7) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+	
+	public String toString() {
+		return nomeDisciplina + " " + horas + " " + media() + " " + Arrays.toString(notas);
+	}
+	
 }
