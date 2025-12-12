94afb127f4a3e55017b47ec5fa0b7f4110865242
Kaio0708 <kaiovitor0707@gmail.com>
2025-12-12 11:08:14 -0300

    Envio dos testes finais

--- teste/lab3FilmNow/DetalharFilmeTest.java
+++ teste/lab3FilmNow/DetalharFilmeTest.java
@@ -4,11 +4,93 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import filmnow.DetalharFilme;
+import filmnow.FilmNow;
+import filmnow.Filme;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 class DetalharFilmeTest {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
+	private FilmNow fn;
+	
+	@BeforeEach
+	void preparaDetalharFilme() {
+		fn = new FilmNow();
 	}
+	
+	@Test
+	void testarDetalharFilmeTodosDados() {
+		fn.cadastrarFilme(5, "Avatar", "2009", "Disney");
+		Filme filme = fn.getFilme(5);
+		DetalharFilme df = new DetalharFilme(filme, 5);
+		assertEquals(df.toString(), "\n" + filme.getNome() + ", " + filme.getAno() + "\n" + df.locaisDeExibicao());
+	}
+	
+	@Test
+	void testarDetalharFilmeSemAnoDeLancamento() {
+		fn.cadastrarFilme(1, "20 dias em Mariupol", "", "Cinema");
+		Filme filme = fn.getFilme(1);
+		DetalharFilme df = new DetalharFilme(filme, 1);
+		assertEquals(df.toString(), "\n" + filme.getNome() + "\n" + df.locaisDeExibicao());
+	}
+	
+	@Test
+	void testarDatalharFilmeEmUmaPosicaoSemFilme() {
+		DetalharFilme df = new DetalharFilme(null,5);
+		assertEquals(df.toString(),"");
+	}
+	
+	@Test
+	void testarDetalharFilmePosicaoAbaixoDoLimite() {
+		DetalharFilme df = new DetalharFilme(null,0);
+		assertEquals(df.toString(), "POSIÇÃO INVÁLIDA!");
+	}
+	
+	@Test
+	void testarDetalharFilmePosicaoAcimaDoLimite() {
+		DetalharFilme df = new DetalharFilme(null,101);
+		assertEquals(df.toString(), "POSIÇÃO INVÁLIDA!");
+	}
+	
+	@Test
+	void testarDetalharFilmeHotList() {
+		fn.cadastrarFilme(5, "Avatar", "2009", "Disney+");
+		Filme filme = fn.getFilme(5);
+		fn.atribuirHot(filme, 1);
+		DetalharFilme df = new DetalharFilme(filme,5);
+		assertEquals(df.toString(), "\n" + "🔥" + filme.getNome() + ", " + filme.getAno() + "\n" + df.locaisDeExibicao());
+	}
+	
+	@Test 
+	void testarDetalharFilmeHotListSemLocalDeExibicao(){
+		fn.cadastrarFilme(5, "Poderoso chefao", "1950", "");
+		Filme filme = fn.getFilme(5);
+		if (filme != null) {
+	        fn.atribuirHot(filme, 1);
+	    }
+		DetalharFilme df = new DetalharFilme(filme, 5);
+		assertEquals("", df.toString());
+	}
+	
+	@Test 
+	void testarDetalharFilmeHotListSemAno(){
+		fn.cadastrarFilme(5, "Poderoso chefao", "", "Amazon");
+		Filme filme = fn.getFilme(5);
+		fn.atribuirHot(filme, 1);
+		DetalharFilme df = new DetalharFilme(filme, 5);
+		assertEquals(df.toString(), "\n" + "🔥" + filme.getNome() + "\n" + df.locaisDeExibicao());
+	}
+	
+	@Test 
+	void testarDetalharFilmeHotListComAno(){
+		fn.cadastrarFilme(2, "Poderoso chefao", "1950", "Amazon");
+		Filme filme = fn.getFilme(2);
+		fn.atribuirHot(filme, 1);
+		DetalharFilme df = new DetalharFilme(filme, 2);
+		String esperado = "\n🔥Poderoso chefao, 1950\nAmazon";
 
+	    assertEquals(esperado, df.toString());
+	}
 }
--- teste/lab3FilmNow/FilmeTest.java
+++ teste/lab3FilmNow/FilmeTest.java
@@ -17,5 +17,78 @@ class FilmeTest {
 		assertEquals(filme.adicionarLocal("Amazon"), true);
 	}
 	
+	@Test
+	void testarToString() {
+		Filme filme = new Filme("Poderoso chefao",1950,"Disney+");
+		assertEquals(filme.toString(), filme.getNome());
+	} 
 	
+	@Test
+	public void testeNomeNull() {
+		try {
+			Filme filme = new Filme(null,1950,"Disney+");
+			if (filme.getNome() == null) {
+				throw new IllegalArgumentException("FILME NULO");
+			}
+		} catch (IllegalArgumentException npe) {
+			assertEquals("Nome não é válido.", npe.getMessage());
+		}
+	}
+	
+	@Test
+	public void testeNomeVazio(){
+		try {
+			Filme filme = new Filme("",1950,"Disney+");
+			if ("".equals(filme.getNome().trim())) {
+				throw new IllegalArgumentException("Nome não é válido.");
+			}
+		} catch (IllegalArgumentException iae) {
+			assertEquals("Nome não é válido.", iae.getMessage());
+		}
+	}
+	
+	@Test
+	void testarAdicionarLocalComListaLotada() {
+		Filme filme = new Filme("Poderoso chefao",1950,"Amazon");
+		filme.adicionarLocal("Amazon Prime Video");
+		filme.adicionarLocal("GloboPlay");
+		filme.adicionarLocal("HBO");
+		filme.adicionarLocal("Crunchyroll");
+		assertEquals(filme.adicionarLocal("netflix"), false);
+	}
+	
+	@Test
+	void testarAdicionarLocalComMesmoNome() {
+		Filme filme = new Filme("Poderoso chefao",1950,"Amazon");
+		assertEquals(filme.adicionarLocal("Amazon"), false);
+	}
+	
+	@Test
+	void testarRemoverLocal() {
+		Filme filme = new Filme("Poderoso chefao",1950,"Amazon");
+		filme.adicionarLocal("GloboPlay");
+		assertEquals(filme.removerLocal("Amazon"), true);
+	}
+	
+	@Test
+	void testarRemoverLocalComNomeErrado() {
+		Filme filme = new Filme("Poderoso chefao",1950,"Amazon");
+		filme.adicionarLocal("GloboPlay");
+		assertEquals(filme.removerLocal("Disney"), false);
+	}
+	
+	@Test
+	void testarRemoverLocalComApenasUmLocalNaLista() {
+		Filme filme = new Filme("Poderoso chefao",1950,"Amazon");
+		assertEquals(filme.removerLocal("Amazon"), false);
+	}
+	
+	@Test
+	void testarQuantidadeDeLocais() {
+		Filme filme = new Filme("Avatar",2009,"Disney+");
+		filme.adicionarLocal("Amazon");
+		filme.adicionarLocal("Netflix");
+		assertEquals(filme.quantidadeDeLocais(), 3);
+	}
+
 }
