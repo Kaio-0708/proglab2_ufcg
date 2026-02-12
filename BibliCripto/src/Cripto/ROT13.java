ad5c49b0a4679c2504c7235cc2a85fef00cbea12
Kaio0708 <kaiovitor0707@gmail.com>
2026-02-12 17:22:58 -0300

    implementação de lógica do ROT13

--- BibliCripto/src/Cripto/ROT13.java
+++ BibliCripto/src/Cripto/ROT13.java
@@ -3,6 +3,22 @@ package Cripto;
 public class ROT13 implements Criptografia{
 	@Override
 	public String criptografar(String texto) {
-		return null;
+		
+		String resultado = "";
+		
+		for(int i = 0; i < texto.length(); i++) {
+			char letra = texto.charAt(i);
+			
+			if(letra >= 'a' && letra <= 'z') {
+				letra = (char) (letra + 13);
+				
+				if(letra > 'z') {
+					letra = (char) (letra - 26);
+				}
+			}
+			resultado = resultado + letra;
+		}
+		
+		return resultado;
 	}
 }
