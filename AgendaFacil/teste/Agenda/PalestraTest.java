dc028b9b1181b086f507dd9e117b684c39ee24a8
Kaio0708 <kaiovitor0707@gmail.com>
2026-02-23 19:50:03 -0300

    feat: add test Palestra

--- AgendaFacil/teste/Agenda/PalestraTest.java
+++ AgendaFacil/teste/Agenda/PalestraTest.java
@@ -2,13 +2,22 @@ package Agenda;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class PalestraTest {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
+	private Palestra palestra;
+	
+	@BeforeEach
+	void setUp() {
+		palestra = new Palestra("Java", "Teste de Java", "07/08/2001", 10);
 	}
 
+	@Test
+	void validarCalculoPontuacao() {
+		int valor = palestra.calcularPontuacao();
+		
+		assertEquals(2, valor);
+	}
 }
