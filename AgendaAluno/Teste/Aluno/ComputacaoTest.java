661f2d17417b747e2f4c32de4e34763673631567
Kaio0708 <kaiovitor0707@gmail.com>
2026-03-03 21:55:40 -0300

    feat: add tests of computacao and informatica

--- AgendaAluno/Teste/Aluno/ComputacaoTest.java
+++ AgendaAluno/Teste/Aluno/ComputacaoTest.java
@@ -2,13 +2,29 @@ package Aluno;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class ComputacaoTest {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
+	private Computacao computacao;
+	
+	@BeforeEach
+	void setUp() {
+		computacao = new Computacao("Robotica", "robotica com IA", "0000", 10);
 	}
-
+    
+	@Test
+	void calcularPontuacao() {
+		int resultado = computacao.calcularPontuacao();
+		
+		assertEquals(2, resultado);
+	}
+	
+	@Test
+	void naoCalcularPontuacao() {
+		int resultado = computacao.calcularPontuacao();
+		
+		assertNotEquals(3, resultado);
+	}
 }
--- AgendaAluno/Teste/Aluno/InformaticaTest.java
+++ AgendaAluno/Teste/Aluno/InformaticaTest.java
@@ -2,13 +2,29 @@ package Aluno;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class InformaticaTest {
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
+	private Informatica informatica;
+	
+	@BeforeEach
+	void setUp() {
+		informatica = new Informatica("Robotica", "robotica com IA", "0000", 10, 20);
 	}
-
+	
+	@Test
+	void calcularPontuacao() {
+		int resultado = informatica.calcularPontuacao();
+		
+		assertEquals(20, resultado);
+	}
+	
+	@Test
+	void naoCalcularPontuacao() {
+		int resultado = informatica.calcularPontuacao();
+		
+		assertNotEquals(10, resultado);
+	}
 }
